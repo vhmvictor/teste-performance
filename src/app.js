32b1ad9b6e0c61206e444acde09a0ec2176727b7
vhmvictor <victor.hugomarques@hotmail.com>
2020-05-03 12:50:27 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const { https } = require("follow-redirects");
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const dotenv = require("dotenv");
 
 dotenv.config();
 app.use(express.json());
+app.use(cors());
 
 app.post("/test", async (request, response) => {
 
