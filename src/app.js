b53795a00a9d11fecaa83fcf6a978cb7c974b23d
vhmvictor <victor.hugomarques@hotmail.com>
2020-05-06 22:25:55 -0300

    update

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const { http } = require("follow-redirects");
+const axios = require('axios');
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -11,56 +12,54 @@ app.use(cors());
 app.get("/hello", (request, response) => {
     response.json({ message: 'Hello Heroku' })
 })
-
+//
 app.post("/test", async (request, response) => {
 
     const { url, email, telefone } = request.body;
 
-    console.log(url)
-    console.log(email)
-    console.log(telefone)
+    console.log(url);
+    console.log(email);
+    console.log(telefone);
 
     const dominio = url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "").split("/")[0]
-
+    
     const gtmetrix = require("gtmetrix")({
         email: process.env.DF_EMAIL,
         apikey: process.env.DF_API_KEY,
         timeout: 5000
     });
-
+    //
     try {
-
+        const urlApi = process.env.DF_IMG_API + dominio
+        const img = (await axios({ url: urlApi,method: 'GET' })).data.lighthouseResult.audits["final-screenshot"].details.data
+        //
         http.get("http://" + dominio, async res => {
-
             const protocol = res.req._redirectable._options.protocol;
             const hostname = res.req._redirectable._options.hostname;
             const currentUrl = res.req._redirectable._currentUrl;
             const isRedirect = res.req._redirectable._isRedirect;
-
             const gtCreateResponse = await gtmetrix.test.create({url: hostname, location: 6, browser: 3})
-                const gtDetails = await gtmetrix.test.get(gtCreateResponse.test_id, 2000)
-                    const gtResource = await gtmetrix.test.get(gtCreateResponse.test_id, process.env.DF_RESOURCE, 2000)
-                    const gtImg = await gtmetrix.test.get(gtCreateResponse.test_id, "screenshot", 2000)
-                    console.log(gtImg)
-
+            const gtDetails = await gtmetrix.test.get(gtCreateResponse.test_id, 2000)
+            const gtResource = await gtmetrix.test.get(gtCreateResponse.test_id, process.env.DF_RESOURCE, 2000)
+            //
             if(protocol == "http:") {
                 return response.json({
                     message: "O dominio " + dominio + " não possui certificado SSL/TLS",
                     protocol: "http",
                     currentUrl: currentUrl,
                     hostname: hostname,
-                    img: gtImg,
+                    img: img,
                     gtCreateResponse: gtCreateResponse,
                     gtDetails: gtDetails,
                     gtResource: gtResource
                 }); 
             }
-
+            //
             return response.json({ 
                 protocol, 
                 hostname, 
                 currentUrl,
-                img: gtImg,
+                img,
                 isRedirect, 
                 gtCreateResponse, 
                 gtDetails, 
@@ -74,12 +73,13 @@ app.post("/test", async (request, response) => {
                 protocol: "http"
             });
         });
-
+        //
     } catch (error) {
         console.log(error);
     }
+    
 });
-
+//
 app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor iniciado!");
 });
